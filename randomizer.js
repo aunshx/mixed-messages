e60a3ef4fceca89c8e744fa5cd83ac3e44b1a4a9
aunshx <aunsh.tech@gmail.com>
2021-06-29 21:04:04 +0530

    Randomizer Basic Edit

--- randomizer.js
+++ randomizer.js
@@ -2,9 +2,16 @@
 This module will be exported to the main code viz. main.js
 */
 
-const feelingsArray = ['love', 'hate'];
+const feelingsArray = ['loves', 'hates'];
 const seasonsArray = ['rainy', 'winter', 'summer'];
 const conjunctionsArray = ['and', 'but'];
+
+/*
+The arrays below are the responses required when a combination of feelings + seasons + conjuctions are selected.
+The first sentence in the array is if the conjuction is 'and' and the second sentence is if the conjuction is 'but'.
+For eg: Erika *loves* the *rainy* season *but* hatesRainySeasonResponse[1]. 
+If it were *and* then it would be hatesRainySeasonResponse[0]. 
+*/
 
 const lovesRainySeasonResponses = ['goes for long walks in the rain.', 'can do without all the water clogging in the city'];
 const hatesRainySeasonResponses = ['does not like getting out of the house.','a cup of hot coffee usually makes things right.'];
@@ -14,3 +21,21 @@ const hatesWinterSeasonResponses = ['wishes it to be over as fast as possible.',
 
 const lovesSummerSeasonResponses = ["can't get enough of it!", 'the mosquitoes always prove to be a menace.'];
 const hatesSummerSeasonResponses = ['the traditional family vactions that come with it.', 'a swim in the cool waters of the village lake is the best feeling ever!'];
+
+const randomSelector = () => {
+    let randomAnswerArray = [];
+
+    let randomFeelings = feelingsArray[Math.floor(Math.random() * 2)];
+    let randomSeasons = seasonsArray[Math.floor(Math.random() * 3)];
+    let randomConjunctions = conjunctionsArray[Math.floor(Math.random() * 2)];
+    
+    return randomAnswerArray[randomFeelings,randomSeasons,randomConjunctions];
+}
+
+const completeSentence = (randomSelection) => {
+    let computeSentence = randomSelection();
+    
+    if(computeSentence[0] === 'loves') {
+        if(computeSentence[1] === 'rainy')
+    }
+}
