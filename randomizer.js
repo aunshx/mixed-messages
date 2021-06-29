a0e1fda3f623ba3356b16cf29032706257c3deaa
aunshx <aunsh.tech@gmail.com>
2021-06-29 20:36:45 +0530

    Adding Randomizer

---
+++ randomizer.js
@@ -0,0 +1,16 @@
+/*This code is to randomize the input paramters viz.  feelings, seasons, conjuctions and their responses. 
+This module will be exported to the main code viz. main.js
+*/
+
+const feelingsArray = ['love', 'hate'];
+const seasonsArray = ['rainy', 'winter', 'summer'];
+const conjunctionsArray = ['and', 'but'];
+
+const lovesRainySeasonResponses = ['goes for long walks in the rain.', 'can do without all the water clogging in the city'];
+const hatesRainySeasonResponses = ['does not like getting out of the house.','a cup of hot coffee usually makes things right.'];
+
+const lovesWinterSeasonResponses = ['the fresh hot chocolate that mom makes.', 'the temperature makes it unbearable sometimes.'];
+const hatesWinterSeasonResponses = ['wishes it to be over as fast as possible.', 'Christmas makes it all worth while.'];
+
+const lovesSummerSeasonResponses = ["can't get enough of it!", 'the mosquitoes always prove to be a menace.'];
+const hatesSummerSeasonResponses = ['the traditional family vactions that come with it.', 'a swim in the cool waters of the village lake is the best feeling ever!'];
---
+++ script.js
@@ -0,0 +1,5 @@
+const personalNouns = ['I', 'Me','You', 'They', 'Them', 'It'];
+
+const whatIsYourName = varName => {
+    console.log(`Hi ${varName}! Nice to meet you. Let's have some fun ;)`);
+}
